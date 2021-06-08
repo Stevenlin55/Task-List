a10f41ae59dc73ec33e43adbb478f6cccbeda017
Stevenlin55 <70299036+Stevenlin55@users.noreply.github.com>
2021-06-08 12:01:53 -0500

    Started implementing firebase and routing

--- task-list-react/src/App.js
+++ task-list-react/src/App.js
@@ -1,11 +1,14 @@
 import { Component } from 'react';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 import TaskForm from './components/TaskForm';
 import TaskTable from './components/TaskTable';
 import CompletedTaskTable from './components/CompletedTaskTable'
 import Task from './models/task';
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 
 class App extends Component {
 
@@ -47,27 +50,25 @@ class App extends Component {
   }
 
   onTaskUpdated(task) {
-    console.log(task.isComplete)
-    if (task.isComplete) {  
-      for(let i = 0; i < this.state.tasks.length; i++) {
-        if (this.state.tasks[i].id === task.id) {
-          this.state.completedTasks.push(this.state.tasks[i]);
+
+    if (task.isComplete) { //if a task is now completed
+      for(let i = 0; i < this.state.tasks.length; i++) { //loop through the unfinished tasks
+        if (this.state.tasks[i].id === task.id) { //find the task that was recently completed using id
+          this.state.completedTasks.push(this.state.tasks[i]); //the completedTask array will add the task
           this.setState({
-            tasks: this.state.tasks.filter(x => x.id !== task.id),
-            completedTasks: this.state.completedTasks
+            tasks: this.state.tasks.filter(x => x.id !== task.id), //the unfinished tasks array will filter out the completed task
+            completedTasks: this.state.completedTasks //completedTasks updates
           });
         }
       }
       
     } else {
-      for(let i = 0; i < this.state.completedTasks.length; i++) {
+      for(let i = 0; i < this.state.completedTasks.length; i++) {  //if a task is not completed anymore, loop through finished tasks
         if (this.state.completedTasks[i].id === task.id) {
-          this.state.tasks.push(this.state.completedTasks[i]);
+          this.state.tasks.push(this.state.completedTasks[i]); //unfinished tasks array adds the task
           this.setState({
             tasks: this.state.tasks,
             completedTasks: this.state.completedTasks.filter(x => x.id !== task.id)
-            //tasks: this.state.tasks.filter(x => x.id !== task.id),
-            //completedTasks: this.state.completedTasks
           });
         }
       }
@@ -77,6 +78,14 @@ class App extends Component {
 
   render() {
     return (
+
+      
+      <BrowserRouter>
+      <Route path="/" exact component={Home}/>      
+      <Route path="/login" exact component={Login}/>      
+      <Route path="/register" exact component={Register}/>      
+    </BrowserRouter>
+    
       <div className="container mt-3">
         <div className="card card-body">
 
@@ -94,6 +103,7 @@ class App extends Component {
           />
         </div>
       </div>
+      
     );
   }
 }
